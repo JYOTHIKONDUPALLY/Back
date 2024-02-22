6803b604ac79d8713b2b2c87cae814ab93d944ec
Annem jyothi <jyothi@gmail.com>
2024-02-22 16:08:36 +0530

    changes

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const http=require("http");
-
+const http =require("http");
 const Port=8081;
-
-//.createServer is a function which create a server
+//Create a server and send a html , for request of status send serevr status
 const server=http.createServer((req, res)=>{
-console.log("server created")
-const date=new Date().toLocaleDateString();
-const time=new Date().toLocaleTimeString();
-console.log(date, time);
-res.write("hello from jyothi");
+if(req.url==='/status'){
+    const date=new Date().toLocaleDateString();
+    const time=new Date().toLocaleTimeString();
+    const serverStatus={
+        name:"jyothi",
+        version:"4.5",
+        date,
+        time
+    }
+    res.writeHead(200,{"Content-Type":"application/json"})
+    res.write(JSON.stringify(serverStatus));
+}
+else{
+    res.write("<h1>Hi from server</h1>")
+}
 res.end();
 });
 
-//.listen is a function used for server to listen
-server.listen(Port,()=>{
-    console.log("server is running in 8081 port")
-})
+server.listen(Port, ()=>{
+console.log(`server is running in 8081`);
+});
